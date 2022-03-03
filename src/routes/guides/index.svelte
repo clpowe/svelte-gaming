c2557ceeb42e895b68de62dc456800c5f3c6e6e9
clpowe <clpowe@gmail.com>
2022-03-03 09:44:18 -0500

    api

--- src/routes/guides/index.svelte
+++ src/routes/guides/index.svelte
@@ -1,11 +1,34 @@
+<script context="module">
+  export async function load({fetch}) {
+    const res = await fetch('/guides.json')
+    const { guides } = await res.json()
+
+    if (res.ok) {
+      return {
+        props: {
+          guides
+        }
+      }
+    }
+
+    return {
+      status: res.status,
+      error: new Error('Could not fetch the guides')
+    }
+  }
+</script>
+
+<script>
+  export let guides
+</script>
+
 <div class="guides">
   <ul>
-    <li>
-      <a href="/">guide 1</a>
-    </li>
-    <li>
-      <a href="/">guide 2</a>
-    </li>
+    {#each guides as guide}
+      <li>
+        <a sveltekit:prefetch href={`/guides/${guide.id}`}>{guide.title}</a>
+      </li>
+    {/each}
   </ul>
 </div>
 
